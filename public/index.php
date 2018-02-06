dba16dc356e947493a8b548da1b6c7417fdf387b
andrii <kaa89pl@gmail.com>
2018-02-06 15:05:00 +0200

    func getLang

--- public/index.php
+++ public/index.php
@@ -6,7 +6,24 @@
  * Time: 14:32
  */
 
+function getLang(array $get, array $cookie, array $session, array $server, $default) {
+    return
+        !empty($get['lang']) ? $get['lang'] :
+            (!empty($cookie['lang']) ? $cookie['lang'] :
+                (!empty($session['lang']) ? $session['lang'] :
+                    (!empty($server['HTTP_ACCEPT_LANGUAGE']) ?substr($server['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
+                    $default
+                    )
+                )
+            );
+}
+
+session_start();
+
 $name = $_GET['name'] ?? 'Guest';
+$lang = getLang($_GET,$_COOKIE, $_SESSION, $_SERVER, 'en');
+
+
 header('X-Developer: KAA');
 
-echo 'Hello! '.$name.'!'.PHP_EOL;
+echo 'Hello! '.$name.'! Your lang is '.$lang.PHP_EOL;
